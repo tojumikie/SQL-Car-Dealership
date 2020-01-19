a0d6fc700d84f3ca5574c5caebe04e505cd82667
toju mikie <tojumikie@live.com>
2020-01-19 12:24:29 -0500

    working on adding and removing cars

---
+++ Project/src/main/java/com/revature/tojudealership/sqlDealership/EmployeeLogin.java
@@ -0,0 +1,62 @@
+package com.revature.tojudealership.sqlDealership;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class EmployeeLogin {
+	
+	public void addOrRemoveCars() {
+		
+	}
+	
+	public static void displayCars() {
+		DriverClass.SQLConnect();
+		try (Connection conn = DriverManager.getConnection(DriverClass.URL, DriverClass.USERNAME, DriverClass.PASSWORD)) {
+			String sql = "select * from cars";
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				System.out.println(rs.getString("name") + "\t" + rs.getString("price"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		displayCars();
+		String decision;
+		String carName;
+		int carPrice;
+		String sql;
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Do you wish to add or remove a car to the lot?");
+		decision = kb.nextLine();
+		if(decision.equals("add")) {
+			//System.out.println("add");
+			System.out.println("Type in the name of the car that you would like to add.");
+			carName = kb.nextLine();
+			System.out.println("Type in the price of the car that you would like to add.");
+			carPrice = kb.nextInt();
+			sql = "insert into cars (name, price) values ('" + carName + "'" + "," + "'" + carPrice + "');";
+			//System.out.println(sql);
+			try (Connection conn = DriverManager.getConnection(DriverClass.URL, DriverClass.USERNAME, DriverClass.PASSWORD)) {
+				Statement stmt = conn.createStatement();
+				stmt.executeUpdate(sql);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else if(decision.equals("remove")) {
+			//System.out.println("remove");
+			System.out.println("Type in the name of the car that you would like to remove.");
+			carName = kb.nextLine();
+		}
+	}
+}
