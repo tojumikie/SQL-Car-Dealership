5d1bd3387b7f7f1bf450cd486e766ef00817e675
toju mikie <tojumikie@live.com>
2020-01-19 09:55:25 -0500

    added working user login

--- Project/src/main/java/com/revature/tojudealership/sqlDealership/UserLogin.java
+++ Project/src/main/java/com/revature/tojudealership/sqlDealership/UserLogin.java
@@ -2,6 +2,7 @@ package com.revature.tojudealership.sqlDealership;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -38,7 +39,34 @@ public class UserLogin {
 			}
 		}
 		else if(response.equalsIgnoreCase("log in")) {
-			
+			boolean tf;
+			System.out.println("Enter the username");
+			u = kb.nextLine();
+			System.out.println("Enter the password");
+			p = kb.nextLine();
+			String sql = "SELECT * FROM login WHERE username = '" + u + "' and \"password\" = '" + p + "';";
+			try (Connection conn = DriverManager.getConnection(DriverClass.URL, DriverClass.USERNAME, DriverClass.PASSWORD)) {
+				if(conn != null) {
+					Statement stmt = conn.createStatement();
+					ResultSet rs = stmt.executeQuery(sql);
+					tf = rs.next();
+					if(tf == true) {
+						System.out.println("Account found. Logging in.");
+						System.out.println("Welcome, " + u);
+					}
+					else {
+						System.out.println("Unable to log in. Username or password "
+								+ "is incorrect");
+					}
+				} 
+				else {
+					System.out.println("not connected");
+				}
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 	
@@ -47,6 +75,11 @@ public class UserLogin {
 //		String p = "12345678";
 //		String sql = "insert into login (username, password) values ('" + u + "'" + "," + "'" + p + "');";
 //		System.out.println(sql);
+//		String u = "max";
+//		String p = "1234";
+//		String sql = "SELECT * FROM login WHERE username = '" + u + "' and \"password\" = '" + p + "';";
+//		System.out.println(sql);
+		
 	}
 }
 
